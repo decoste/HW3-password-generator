19dea3e79d7c6a063eb24e598ceb15c6ca055e1d
ala decoste <alshareefialaa@gmail.com>
2020-10-09 23:49:33 -0400

    a series of prompts for password criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 let UpperCase =
-    ['A',
+    [
+        'A',
         'B',
         'C',
         'D',
@@ -27,7 +28,8 @@ let UpperCase =
     ];
 
 let numbers =
-    ['0',
+    [
+        '0',
         '1',
         '2',
         '3',
@@ -40,7 +42,8 @@ let numbers =
     ];
 
 let specialCharacters =
-    ['@',
+    [
+        '@',
         '#',
         '$',
         '%',
@@ -70,3 +73,31 @@ for (i = 0; i < UpperCase.length; i++) {
     lowerCase.push(UpperCase[i].toLowerCase());
 }
 console.log(lowerCase);
+
+function PassOptions() {
+    let length = parseInt(prompt('How many characters would you like your password to contain?Your password should be between 8 to 128 charecters'));
+    if (isNaN(length) === true) {
+        alert("Please enter a number.");
+        return;
+    }
+    if (length < 8 || length > 128) {
+        alert('Password length must be between 8 and 128 characters');
+        return;
+    }
+
+    let includeSpecialChar = confirm('Click OK to confirm including special characters.');
+    let includeNumeric = confirm('Click OK to confirm including numeric characters.');
+    let includeLowerCase = confirm('Click OK to confirm including lowercase characters.');
+    let includeUpperCase = confirm('Click OK to confirm including uppercase characters.');
+
+    if (includeSpecialChar === false
+        && includeNumeric === false
+        && includeLowerCase === false
+        && includeUpperCase === false) {
+        alert('The user must select at least one character type.')
+    }
+}
+
+
+
+PassOptions();
